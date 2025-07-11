e71603570ccd3050a8f8f2cb39200d70ea07241e
rdalx42 <hopartean.radu@spectrum.ro>
2025-07-11 21:01:58 +0300

    Update main.cpp
    Added a working I/O system

--- zLang/main.cpp
+++ zLang/main.cpp
@@ -1,26 +1,45 @@
-
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 #include "input_parse.h"
 
 std::ifstream input("input.cbp");
 
-
 int main(void) {
+
     if (!input) {
-        std::cout << "Couldn't read - input doesn't exist.\n";
         return 0;
     }
 
+    std::cout << "\n======== [WRITE PROGRAM INPUT - PRESS CTRL + C AND ENTER TO FINISH] ========\n";
+
+    // get code input from file
+
     std::string ln;
+
     while (std::getline(input, ln)) {
         zlang_input[lines] = ln;
         ++lines;
     }
 
-    validate_input();
+    // get user input from console and use it as zlang program input
+
+    std::string word;
+
+    while (std::cin >> word) {
+        program_input[input_lines] = word;
+        ++input_lines;
+    }
+
+    std::cout<<"\n======== [CODE INPUT] ========\n\n";
+
+    output_program_input();
+
+    std::cout<<"\n======== [ZLANG OUTPUT] ========\n";
+
+    RUN_ZLANG(); // run code after input was written
 
     return 0;
 }
