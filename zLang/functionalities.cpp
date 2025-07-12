c5015e0a5578969c2c758cd072ffb59da02b5bb2
rdalx42 <hopartean.radu@spectrum.ro>
2025-07-12 19:30:18 +0300

    Update functionalities.cpp
    organized some code

--- zLang/functionalities.cpp
+++ zLang/functionalities.cpp
@@ -5,108 +5,7 @@
 #include <sstream>
 #include "input_parse.h"
 #include "data.h"
-
-int operation_statement(std::string x, std::string op, std::string y) {
-    int val_x = 0;
-    int val_y = 0;
-
-    if (is_num(x)) {
-        val_x = std::stoi(x);
-    } else if (get_num_val(x) != INT_MAX) {
-        val_x = num_vars[x];
-    } else {
-        return 0;
-    }
-
-    if (y.empty()) {
-        return val_x;
-    }
-
-    if (is_num(y)) {
-        val_y = std::stoi(y);
-    } else if (get_num_val(y) != INT_MAX) {
-        val_y = num_vars[y];
-    } else {
-        return 0;
-    }
-
-    if (op == "+=" && get_num_val(x) != INT_MAX) {
-        num_vars[x] += val_y;
-        return num_vars[x];
-    }
-    if (op == "-=" && get_num_val(x) != INT_MAX) {
-        num_vars[x] -= val_y;
-        return num_vars[x];
-    }
-    if (op == "*=" && get_num_val(x) != INT_MAX) {
-        num_vars[x] *= val_y;
-        return num_vars[x];
-    }
-    if (op == "/=") {
-        if (val_y == 0) {
-            std::cout << "Division by zero error\n";
-            return 0;
-        }
-        if (get_num_val(x) != INT_MAX) {
-            num_vars[x] /= val_y;
-            return num_vars[x];
-        }
-    }
-    if (op == "%=") {
-        if (val_y == 0) {
-            std::cout << "Modulo by zero error\n";
-            return 0;
-        }
-        if (get_num_val(x) != INT_MAX) {
-            num_vars[x] %= val_y;
-            return num_vars[x];
-        }
-    }
-    if (op == "&=" && get_num_val(x) != INT_MAX) {
-        num_vars[x] &= val_y;
-        return num_vars[x];
-    }
-    if (op == "|=" && get_num_val(x) != INT_MAX) {
-        num_vars[x] |= val_y;
-        return num_vars[x];
-    }
-    if (op == "^=" && get_num_val(x) != INT_MAX) {
-        num_vars[x] ^= val_y;
-        return num_vars[x];
-    }
-    if (op == "<<=" && get_num_val(x) != INT_MAX) {
-        num_vars[x] <<= val_y;
-        return num_vars[x];
-    }
-    if (op == ">>=" && get_num_val(x) != INT_MAX) {
-        num_vars[x] >>= val_y;
-        return num_vars[x];
-    }
-    if (op == "+") return val_x + val_y;
-    if (op == "-") return val_x - val_y;
-    if (op == "*") return val_x * val_y;
-    if (op == "/") {
-        if (val_y == 0) {
-            std::cout << "Division by zero error\n";
-            return 0;
-        }
-        return val_x / val_y;
-    }
-    if (op == "%") {
-        if (val_y == 0) {
-            std::cout << "Modulo by zero error\n";
-            return 0;
-        }
-        return val_x % val_y;
-    }
-    if (op == "&") return val_x & val_y;
-    if (op == "|") return val_x | val_y;
-    if (op == "^") return val_x ^ val_y;
-    if (op == "<<") return val_x << val_y;
-    if (op == ">>") return val_x >> val_y;
-
-    return LLONG_MAX;
-}
+#include "statemenets.h"
 
 void proccess_line(std::string& x) {
     std::stringstream ss(x);
@@ -121,6 +20,11 @@ void proccess_line(std::string& x) {
             z = "";
         }
 
+        if (is_variable(name)) {
+            std::cout << "VARIABLE ALREADY EXISTS";
+            return;
+        }
+
         if (!name.empty() && eq == "=") {
             int val = y.empty() ? operation_statement(v, "", "") : operation_statement(v, y, z);
             if (val < INT_MAX && val >= INT_MIN) {
@@ -131,6 +35,34 @@ void proccess_line(std::string& x) {
             }
         } else {
             std::cout << "NR COULDN'T BE DECLARED\n";
+            return;
+        }
+    } else if (start_keyword == "bool") {
+        ss >> name >> eq >> v;
+
+        if (!(ss >> y >> z)) {
+            y = "";
+            z = "";
+        }
+
+        if (is_variable(name)) {
+            std::cout << "VARIABLE ALREADY EXISTS";
+            return;
+        }
+
+        if (!name.empty() && eq == "=") {
+            if (v == "true") {
+                bool_vars[name] = true;
+            } else if (v == "false") {
+                bool_vars[name] = false;
+            } else if (bool_vars.find(v) != bool_vars.end()) {
+                // Assign from another bool variable
+                bool_vars[name] = bool_vars[v];
+            } else {
+                bool_vars[name]=bool_statement(v,y,z);
+            }
+        } else {
+            std::cout << "BOOL COULDN'T BE DECLARED\n";
             return;
         }
     } else if (start_keyword == "print") {
@@ -153,16 +85,17 @@ void proccess_line(std::string& x) {
                 while (i < line.size() && !isspace(line[i])) {
                     varname += line[i++];
                 }
-                if (get_num_val(varname) != INT_MAX) {
-                    std::cout << num_vars[varname];
+                if (is_variable(varname)) {
+                    if (get_var_type(varname) == "string type") std::cout << string_vars[varname];
+                    else if (get_var_type(varname) == "bool type")std::cout<<bool_vars[varname];
+                    else if (get_var_type(varname) == "num type") std::cout << num_vars[varname];
                 } else {
-                    std::cout << varname;
+                    std::cout << '$' << varname;
                 }
             } else {
                 std::cout << line[i++];
             }
         }
-
     } else if (start_keyword == "toinput") {
         ss >> z;
         if (is_variable(z)) {
@@ -171,8 +104,20 @@ void proccess_line(std::string& x) {
         } else {
             std::cout << "COULDNT ASSIGN READ VALUE TO NON EXISTENT VARIABLE\n";
         }
-    } else {
+    }else if(start_keyword == "--"){
+        return;
+    }else {
         ss >> op >> z;
         operation_statement(start_keyword, op, z);
+
+        // only a bool or an if statement can have a bool_statement
+
+        if(get_var_type(start_keyword) == "bool type"){
+            std::string Z,Y;
+            ss >> Z >> Y;
+
+            bool_vars[start_keyword]=bool_statement(z,Z,Y);
+
+        }
     }
 }
