4cfc738f71d610e2750aa26543a5bdf58fd20780
rdalx42 <hopartean.radu@spectrum.ro>
2025-07-12 19:32:42 +0300

    Create statements.h

---
+++ zLang/statements.h
@@ -0,0 +1,9 @@
+
+
+#ifndef STATEMENTS_H
+#define STATEMENTS_H
+
+int operation_statement(std::string x, std::string op, std::string y);
+bool bool_statement(std::string x, std::string op, std::string y);
+
+#endif // STATEMENTS_H
