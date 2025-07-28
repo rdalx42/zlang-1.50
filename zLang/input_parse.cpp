a9b36034ac56648fb0d3a4ed6569b9fdb74d8406
rdalx42 <hopartean.radu@spectrum.ro>
2025-07-28 20:15:37 +0300

    Update input_parse.cpp

--- zLang/input_parse.cpp
+++ zLang/input_parse.cpp
@@ -3,7 +3,6 @@
 #include "input_parse.h"
 #include "functionalities.h"
 #include "data.h"
-
 std::string zlang_input[MAXDIM];
 std::string program_input[MAXDIM];
 
@@ -22,7 +21,7 @@ void remove_whitespace(std::string& x) {
 void RUN_ZLANG() {
     for (int i = 0; i < lines; ++i) {
         remove_whitespace(zlang_input[i]);
-        proccess_line(zlang_input[i]);
+        proccess_line(zlang_input[i],i);
     }
 }
 
@@ -34,7 +33,11 @@ void output_input() {
 
 void output_program_input(){
     for(int i = 0 ; i < input_lines; ++ i){
-        std::cout<<program_input[i]<<"\n";
+        std::cout<<"LINE: "<<i+1<<": "<<program_input[i]<<"\n";
+        std::cout<<"--------";
+        for(int j = 0; j < program_input[i].size();++j){
+            std::cout<<"-";
+        }std::cout<<"\n";
     }
 }
 
@@ -47,3 +50,8 @@ bool is_num(std::string& x) {
         return false;
     }
 }
+
+bool is_bool(std::string& x){
+    return(x == "true" || x == "false");
+}
+
