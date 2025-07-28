769aaa45b0c45539f82672f8f92d7bc0001c17f2
rdalx42 <hopartean.radu@spectrum.ro>
2025-07-28 14:24:27 +0300

    Update data.h

--- zLang/data.h
+++ zLang/data.h
@@ -3,11 +3,13 @@
 
 #include <unordered_map>
 #include <string>
+#include <vector>
 
 extern std::unordered_map<std::string, int> num_vars;
 extern std::unordered_map<std::string, std::string> string_vars;
 extern std::unordered_map<std::string, bool> bool_vars;
 extern std::unordered_map<std::string,std::string>fn_contents;
+extern std::unordered_map<std::string, std::vector<std::string>> fn_params;
 
 int get_num_val(std::string& name);
 
@@ -17,5 +19,6 @@ std::string get_var_type(std::string& name);
 
 bool is_variable(std::string& name);
 void set_variable(std::string& name, std::string& value);
+void set_variable_to_data(std::string& name, std::string& data);
 
 #endif
