b6699f41492e5f1b5a36afc2109edb2369888330
rdalx42 <hopartean.radu@spectrum.ro>
2025-08-13 14:16:37 +0300

    Update data.h

--- zLang/data.h
+++ zLang/data.h
@@ -5,31 +5,50 @@
 #include <string>
 #include <vector>
 
-extern std::unordered_map<std::string, int> num_vars;
-extern std::unordered_map<std::string, std::string> string_vars;
-extern std::unordered_map<std::string, bool> bool_vars;
-extern std::unordered_map<std::string,std::string>fn_contents;
-extern std::unordered_map<std::string, std::vector<std::string>> fn_params;
-extern std::unordered_map<std::string, std::string> fn_values;
+struct var_storage;
+
+extern var_storage GLOBAL_DATA;
+extern std::vector<var_storage> program_data;
+ extern std::unordered_map<std::string, int> num_vars;
+ extern std::unordered_map<std::string, std::string> string_vars;
+ extern std::unordered_map<std::string, bool> bool_vars;
+ //extern std::unordered_map<std::string, std::vector<std::string>> fn_params;
+ //extern std::unordered_map<std::string, std::string> fn_contents;
 
 struct function_read_info{
     std::string name;
-    int start_indx = 0;
-    int end_indx = 0;
+    int start_indx;
+    int end_indx;
+    std::string filename;
 };
 
-extern std::vector<function_read_info>function_read_info_arr;
-
-int get_num_val(std::string& name);
-
-std::string get_bool_val(std::string& name);
-std::string get_string_val(std::string& name);
-std::string get_var_type(std::string& name);
-
-bool is_variable(std::string& name);
-bool is_datatype(std::string& value);
+int get_function_read_info_arr_indx(std::string& fname, const std::string& filename);
+int get_num_val(std::string& name, std::string& filename);
+std::string get_bool_val(std::string& name, std::string& filename);
+std::string get_string_val(std::string& name, std::string& filename);
+std::string get_var_type(std::string& name, std::string& filename);
+void set_variable_to_data(std::string& varname, std::string& data_name);
 void set_variable(std::string& name, std::string& value);
-void set_variable_to_data(std::string& name, std::string& data);
-int get_function_read_info_arr_indx(std::string& fname);
+bool is_datatype(std::string& value);
+int get_index(const std::string& name);
+extern std::vector<function_read_info> function_read_info_arr;
+
+struct var_storage {
+    enum TYPES { NUM, BOOL, NULL_TYPE };
+    struct var_data {
+        std::string name;
+        std::string value;
+        TYPES type;
+        bool constant;
+    };
+    std::unordered_map<std::string, int> name_to_indx;
+    std::vector<var_data> values;
+    std::string name;
+    std::unordered_map<std::string, std::string> fn_values;
+    std::unordered_map<std::string, std::vector<std::string>> fn_params;
+    std::unordered_map<std::string, std::string> fn_contents;
+    void add_variable(const std::string& name, bool constant, const std::string& TYPE, const std::string& VALUE, bool GLOBAL = false);
+    void set_variable(const std::string& name, const std::string& v, bool GLOBAL = false);
+};
 
 #endif
