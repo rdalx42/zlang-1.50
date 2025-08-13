d9aa6d236cc9c847f32f69293cbeb55d45257eeb
rdalx42 <hopartean.radu@spectrum.ro>
2025-08-13 14:18:33 +0300

    Update input_parse.h

--- zLang/input_parse.h
+++ zLang/input_parse.h
@@ -2,6 +2,8 @@
 #define INPUT_PARSE_H
 
 #include <string>
+#include <vector>
+#include "data.h"
 
 #define MAXDIM 10000
 
@@ -11,8 +13,9 @@ extern std::string program_input[MAXDIM];
 extern int lines;
 extern int input_lines;
 extern int current_zlang_input_line;
+extern int var_indx;
 
-void RUN_ZLANG();
+void RUN_ZLANG(const std::string& x);
 void clean_program_input();
 void remove_whitespace(std::string& x);
 void output_input();
@@ -20,6 +23,6 @@ void output_program_input();
 
 bool is_bool(std::string& x);
 bool is_num(std::string& x);
-bool is_fn_value(std::string& x);
+bool is_fn_value(std::string& x, const std::string& filename);
 
 #endif
