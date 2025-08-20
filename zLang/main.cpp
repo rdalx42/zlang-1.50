dd08a22abcbb45e486e8f3b5bd82e23a5eb898d4
rd <hopartean.radu@spectrum.ro>
2025-08-20 18:18:21 +0300

    Update main.cpp

--- zLang/main.cpp
+++ zLang/main.cpp
@@ -8,7 +8,7 @@
 #include "input_parse.h"
 
 std::ifstream input("main.zl");
-std::ifstream zlinput("input.txt");
+std::ifstream zl_input("input.txt");
 
 const char* pattern[] = {
     "@@@@@@@@@@@@@@@@@@@@@",
@@ -24,73 +24,46 @@ const char* pattern[] = {
     "@@@@@@@@@@@@@@@@@@@@@",
 };
 
-const int TERMINAL_WIDTH = 80;
-const int SCALE = 1;
-const int DELAY_MS = 1;
+const int terminal_width = 80;
+const int scale = 1;
 
-void printCharBlock(char c) {
+void print_char_block(char c) {
     const char* color = (c == '@') ? "\033[33m" : "\033[37m"; 
     std::cout << color;
-    for (int i = 0; i < SCALE; ++i)
+    for (int i = 0; i < scale; ++i)
         std::cout << c;
     std::cout << "\033[0m";
 }
 
-void printScaledLine(const char* line) {
+void print_scaled_line(const char* line) {
     int len = 0;
     while (line[len] != '\0') ++len;
 
-    int scaledWidth = len * SCALE;
-    int leftPadding = (TERMINAL_WIDTH - scaledWidth) / 2;
-    if (leftPadding < 0) leftPadding = 0;
+    int scaled_width = len * scale;
+    int left_padding = (terminal_width - scaled_width) / 2;
+    if (left_padding < 0) left_padding = 0;
 
-    for (int v = 0; v < SCALE; ++v) {
-        for (int s = 0; s < leftPadding; ++s) std::cout << ' ';
+    for (int v = 0; v < scale; ++v) {
+        for (int s = 0; s < left_padding; ++s) std::cout << ' ';
         for (int i = 0; i < len; ++i) {
-            printCharBlock(line[i]);
+            print_char_block(line[i]);
         }
         std::cout << '\n';
     }
 }
 
-void animatePatternScaled() {
-    using namespace std::chrono_literals;
-
-    for (int i = 0; i < 3; ++i) std::cout << '\n';
-
+void print_pattern() {
     for (int i = 0; i < 11; ++i) {
-        const char* line = pattern[i];
-        int len = 0;
-        while (line[len] != '\0') ++len;
-
-        int scaledWidth = len * SCALE;
-        int leftPadding = (TERMINAL_WIDTH - scaledWidth) / 2;
-        if (leftPadding < 0) leftPadding = 0;
-
-        for (int v = 0; v < SCALE; ++v) {
-            for (int s = 0; s < leftPadding; ++s) std::cout << ' ';
-
-            for (int i = 0; i < len; ++i) {
-                char c = line[i];
-
-                for (int h = 0; h < SCALE; ++h) {
-                    printCharBlock(c);
-                    std::cout.flush();
-                    std::this_thread::sleep_for(std::chrono::milliseconds(DELAY_MS));
-                }
-            }
-            std::cout << '\n';
-        }
+        print_scaled_line(pattern[i]);
     }
-    std::cout << "\n\n";
 }
 
-int main(void) {
-    
-    animatePatternScaled();
+int main(void){
+   
+    print_pattern();
 
-    if(!input){
-        std::cout<<"couldn't open main.zl file!\n";
+    if (!input) {
+        std::cout << "Couldn't open main.zl file!\n";
         return 0;
     }
 
@@ -106,23 +79,25 @@ int main(void) {
 
     program_data.push_back(zLangProgram{}); 
     program_data[var_indx].name = "main.zl";
-   // program_data[var_indx].has_ran = true;
     RUN_ZLANG("main.zl", get_input_from_file("main.zl"), get_size_from_file("main.zl"));
+    //std::cout << "RUNNING\n";
+    clean_program_input("main.zl");
 
-    var_indx++;
+   // var_indx++;
 
     auto end = std::chrono::high_resolution_clock::now();
 
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
     std::cout << "\n\nProgram runtime: " << duration.count() << " milliseconds\n";
 
-    std::cout<<"\nTYPE 'END' TO EXIT\n";
-    
+    std::cout << "\nTYPE 'END' TO EXIT\n";
+
     while (true) {
         std::getline(std::cin, ln);
         if (ln == "END") break;
         program_input[input_lines] = ln;
         ++input_lines;
     }
+
     return 0;
 }
